17ff70609337d1edfe9319033a03a6184c1a4471
Carlo Varni <carlo.varni@cern.ch>
2019-01-18 11:25:48 +0100

    Adding L1 requirement

--- Python/triggerNameParsing.py
+++ Python/triggerNameParsing.py
@@ -1,7 +1,6 @@
 
 import re
-#re_Bjet = re.compile(r'^HLT_(?P<multiplicity>\d+)?j(?P<threshold>\d+)(?:_gsc(?P<gscThreshold>\d+))?(?:_b(?P<bTag>[^_]+)(?:_(?P<bConfig>split))?)?$')
-re_Bjet = re.compile(r'^HLT_(?P<multiplicity>\d+)?j(?P<threshold>\d+)(?:_gsc(?P<gscThreshold>\d+))?(?:_b(?P<bTag>[^_]+)(?:_(?P<bConfig>split))?(?:_(?P<minEta>\d+)eta(?P<maxEta>\d+))?)?$')
+re_Bjet = re.compile(r'^HLT_(?P<multiplicity>\d+)?j(?P<threshold>\d+)(?:_gsc(?P<gscThreshold>\d+))?(?:_b(?P<bTag>[^_]+)(?:_(?P<bConfig>split))?(?:_(?P<minEta>\d+)eta(?P<maxEta>\d+))?)?(?:_L1(?P<L1>.*))?$')
 
 default_conf = { 'threshold' : '0',
                  'multiplicity' : '1',
@@ -9,7 +8,8 @@ default_conf = { 'threshold' : '0',
                  'bTag' : 'offperf',
                  'bConfig' : 'EF',
                  'minEta' : '0',
-                 'maxEta' : '320'}
+                 'maxEta' : '320',
+                 'L1' : None }
 
 triggerName = [
     "HLT_j35_boffperf",
@@ -27,26 +27,58 @@ triggerName = [
     "HLT_3j35_boffperf_230eta490",
     "HLT_3j35_boffperf_split_230eta490",
     "HLT_3j35_gsc45_boffperf_230eta490",
-    "HLT_3j35_gsc45_boffperf_split_230eta490"
+    "HLT_3j35_gsc45_boffperf_split_230eta490",
+    "HLT_j35_boffperf_L1J15",
+    "HLT_j35_boffperf_split_L1J15",
+    "HLT_j35_gsc45_boffperf_L1J15",
+    "HLT_j35_gsc45_boffperf_split_L1J15",
+    "HLT_j35_boffperf_230eta490_L1J15",
+    "HLT_j35_boffperf_split_230eta490_L1J15",
+    "HLT_j35_gsc45_boffperf_230eta490_L1J15",
+    "HLT_j35_gsc45_boffperf_split_230eta490_L1J15",
+    "HLT_3j35_boffperf_L1J15",
+    "HLT_3j35_boffperf_split_L1J15",
+    "HLT_3j35_gsc45_boffperf_L1J15",
+    "HLT_3j35_gsc45_boffperf_split_L1J15",
+    "HLT_3j35_boffperf_230eta490_L1J15",
+    "HLT_3j35_boffperf_split_230eta490_L1J15",
+    "HLT_3j35_gsc45_boffperf_230eta490_L1J15",
+    "HLT_3j35_gsc45_boffperf_split_230eta490_L1J15",
     ]
 
 referenceConfiguration = [
-    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '0', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '0', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '45', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '45', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '0', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '0', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '45', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '45', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '0', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '0', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '45', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '45', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '0', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '0', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '45', 'threshold': '35'},
-    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '45', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '0', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '0', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '45', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '45', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '0', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '0', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '45', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '45', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '0', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '0', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '45', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '45', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '0', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '0', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '45', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '45', 'L1': None, 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '0', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '0', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '45', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '1', 'minEta': '0', 'gscThreshold': '45', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '0', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '0', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '45', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '1', 'minEta': '230', 'gscThreshold': '45', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '0', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '0', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '45', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '320', 'multiplicity': '3', 'minEta': '0', 'gscThreshold': '45', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '0', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '0', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'EF', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '45', 'L1': 'J15', 'threshold': '35'},
+    {'bTag': 'offperf', 'bConfig': 'split', 'maxEta': '490', 'multiplicity': '3', 'minEta': '230', 'gscThreshold': '45', 'L1': 'J15', 'threshold': '35'},
     ]
 
 def compareConfigurationWithReference( index, conf_dict ):
